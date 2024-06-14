cd84b6bcbd73843805553171c3b85dc751477007
ma2ken12 <167295537+ma2ken12@users.noreply.github.com>
2024-06-14 05:30:54 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session      # ログインの直前に必ずこれを書くこと
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      redirect_to forwarding_url || user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
